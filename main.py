39399fa237f105303efef974286c3913c01e5e09
Hudson <hudson.junior>
2021-03-24 23:28:21 -0300

    fixing some bugs

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from NodeBoard import node_board
-
+import copy
+import heapq as hp
 expected_values = [1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15, 4, 8, 12, 0]
-adjacent_squares = [(-1, -1), (-1, 1), (1, -1), (1, 1), (0, -1), (0, 1), (-1, 0), (1, 0)]
+final_board = [['1', '5', '9', '13'], ['2', '6', '10', '14'], ['3', '7', '11', '15'], ['4', '8', '12', '0']]
 
 A = set() # {}
 F = set() # {}
@@ -28,44 +29,125 @@ def heuristic_h1(node_board):
 
     return count
 
-def get_node_less_total_cost(A):
-    node = min(A, key=lambda node: node.g_cost + node.h_cost)
-    return node
+def get_node_less_total_cost(heap, A):
+    while True:
+        element = hp.heappop(heap)
+        if element[0] in A:
+            return A.get(element[0])
 
-def generate_sucessor(parent_node, board):
-    x, y = parent_node.position
-    children = [board[adj[0]][adj[1]] for adj in [(x-1, y, (x, y-1), (x, y +1), (x + 1, y))]]
-def algorithm_astar(board, start_node, end_node):
-    global adjacent_squares
+def change_part(node, part):
+    new_node = copy.deepcopy(node.board_state)
 
-    A = set()
-    F = set()
+    new_node[node.white_space[0]][node.white_space[1]] = node.board_state[part[0]][part[1]]
+    new_node[part[0]][part[1]] = node.board_state[node.white_space[0]][node.white_space[1]]
+
+    return node_board(new_node, part, node)
+
+def generate_successors(node):
+    white_space = node.white_space
+    print('white white', white_space)
+    children = []
+    ## move to the left
+    if white_space[0] - 1 >= 0:
+        child = change_part(node, (white_space[0] - 1, white_space[1]))
+        child.g_cost = node.g_cost + 1
+        child.h_cost = 0
+        child.parent_node = node
+        children.append(child)
+
+    ## move to the up
+    if white_space[1] - 1 >= 0:
+        child = change_part(node, (white_space[0], white_space[1] - 1))
+        child.g_cost = node.g_cost + 1
+        child.h_cost = 0
+        child.parent_node = node
+        children.append(child)
+
+    ## move to the right
+    if white_space[0] + 1 < 4:
+        child = change_part(node, (white_space[0] + 1, white_space[1]))
+        child.g_cost = node.g_cost + 1
+        child.h_cost = 0
+        child.parent_node = node
+        children.append(child)
+
+    ## move to the down
+    if white_space[1] + 1 < 4:
+        child = change_part(node, (white_space[0], white_space[1] + 1))
+        child.g_cost = node.g_cost + 1
+        child.h_cost = 0
+        child.parent_node = node
+        children.append(child)
+
+    print(children)
+    return children
+
+def get_white_sace(board):
+    for i in range(4):
+        for j in range(4):
+            if (int(board[i][j]) == 0): return (i,j)
+
+def algorithm_astar(initial_board):
+    global final_board
+
+    ## criando estruturas A e F como dicionários para conseguir referenciar melhor
+    A = {}
+    F = {}
+
+    heap_management = []
+
+    # criando uma estrutura heap que servirá para realizar operações otimizadas no A e F
+    hp.heapify(heap_management)
 
     # inicia a lista de nós abertos
-    A.add(start_node)
+    initial_white_space = get_white_sace(initial_board)
+    initial_node = node_board(initial_board, initial_white_space, None)
+
+    A[str(initial_board)] = initial_node
+    hp.heappush(heap_management, (str(initial_board), A[str(initial_board)].g_cost + A[str(initial_board)].h_cost))
+
+    current_node = initial_node
     
-    while len(A):
+    while A:
         ## recuperar o nó com o menor custo total
-        current_node = get_node_less_total_cost(A)
-
-        # remover da lista de nós abertos
-        A.remove(current_node)
-
-        # colocar ele na lista de nós fechados
-        F.add(current_node)
+        current_node = get_node_less_total_cost(heap_management, A)
 
         ## Achou o caminho final, deve construir esse caminho
-        if current_node == end_node:
+        if current_node.board_state == final_board:
             path = []
-
-            while current_node != start_node:
-                path.append((current_node.board_state, current_node.position))
+            while current_node.board_state != initial_board:
+                path.append(current_node.board_state)
                 current_node = current_node.parent_node
 
             return path[::-1]
 
-        for possible_position in adjacent_squares:
-            new_position = (current_node.)
+        A.pop(str(current_node.board_state))
+        F[str(current_node.board_state)] = current_node
+
+        successors_nodes = generate_successors(current_node)
+
+        print('A', A)
+        print('F',F)
+        for successor_node in successors_nodes:
+            print("sucessor", successor_node.board_state)
+            successor_node_converted = str(successor_node.board_state)
+
+            if successor_node_converted in F: continue
+
+            if successor_node_converted not in A and successor_node_converted not in F:
+                print('123')
+                A[successor_node_converted] = successor_node
+                A[successor_node_converted].h_cost = heuristic_h1(successor_node)
+                hp.heappush(heap_management, (successor_node_converted, A[successor_node_converted].g_cost + A[successor_node_converted].h_cost))
+
+            if successor_node_converted in A and A[successor_node_converted].g_cost > successor_node.g_cost:
+                print('456')
+                A.pop(successor_node_converted)
+
+    return None
+                
+
+
 
 
 def main():
@@ -82,11 +164,12 @@ def main():
             board[i][j] = entry[pos]
             pos = pos + 1
 
-    node = node_board(board, 0, 0, None)
-
+    node = node_board(board, (0,0), None)
     out_parts = heuristic_h1(node)
 
-    print(out_parts)
+    print('heuristic 1', out_parts)
+
+    print('a estrela', algorithm_astar(board))
 
 main()
 
